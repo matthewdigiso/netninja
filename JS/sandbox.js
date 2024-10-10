6198d8c466a6161b2d323a9fe06d482bd4f0f25e
Matthew Digiso <126mdigiso@frhsd.com>
2024-10-10 12:43:07 -0400

    ARRAYS

--- JS/sandbox.js
+++ JS/sandbox.js
@@ -1,15 +1,22 @@
-let age = 25;
-let year = 2019;
+let ninjas = ['Shaun', 'ryu', 'chun-li'];
 
-// log things to console
-// console.log(age, year);
+// ninjas[1] = 'ken';
+// console.log(ninjas[1]);
 
-// age = 30;
-// console.log(age);
+// let ages= [20,25,30,35];
+// console.log(ages[2]);
 
-// const points = 100;
-// points = 50;
-// console.log(points);
+// let random=['shaun', 'crystal', 30. 20];
+// console.log(random);
 
-var score = 75;
-console.log(score);
+// console.log(ninjas.length);
+
+// array methods
+
+// let result=ninjas.join('-');
+// let result=ninjas.indexOf('chun-li');
+// let result=ninjas.concat(['ken', 'crystal']);
+let result = ninjas.push('ken');
+result = ninjas.pop();
+
+console.log(result);
